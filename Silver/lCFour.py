f7a8a96d0935990a2a5a0af812f6adb0c6e7aa55
Henry Craig <h.craig52@yahoo.com>
2018-11-04 18:53:28 -0500

    Update lCFour.py

--- Silver/lCFour.py
+++ Silver/lCFour.py
@@ -31,46 +31,68 @@ def lCFour(encryptKey, encryptedMessage):
     markerLocation = 0
     encryptKey = list(encryptKey)
     encryptedMessage = list(encryptedMessage)
+    decryptedString = ''
+
+
+    def findColumn(char):
+        columnLoc = int(char/6)
+        columnarChunk = encryptKey[columnLoc*6:(columnLoc+1)*6]
+
+        if char-columnLoc < (columnLoc+1)*6:
+            columnLocation = char-leftMove+6
+        else:
+            columnLocation = char-leftMove
+
+        print("Column: ", encryptKey[columnLocation])
+
+        return columnLocation
+
+    def findRow(columnLoc):
+        rowLocation = columnLocation - (upMove*6)
+        #and if we want ot to be positive we can do
+        if rowLocation < 0:
+            rowLocation = rowLocation+len(encryptKey)
+        #but do we need to is the question?
+        return rowLocation
 
 
 
-    for i in range(len(encryptKey)):
-        if encryptKey[i] == encryptedMessage[0]:
-            print("Letter: ", encryptedMessage[0], " found at position: ", i, " of the encryptKey")
-            print("Marker Location: ", encryptKey[markerLocation])
-            print("Where the marker letter is in the alphabet: ", cipherAlpha.find(encryptKey[markerLocation]))
 
+    for char in range(len(encryptKey)):
+        if encryptKey[char] == encryptedMessage[0]:
+            print("Letter: ", encryptedMessage[0], " found at position: ", char, " of the encryptKey")
 
 
             #uses the markers location to find the vertical and horizontal movement
             leftMove = cipherAlpha.find(encryptKey[markerLocation])%6
             upMove = int(cipherAlpha.find(encryptKey[markerLocation])/6)
-            print("Modulo: ", leftMove)   #horizontal - for decryption going Left
-            print("Rounded Division: ", upMove)  #vertical - for decryption going Right
-
-
-            columnLoc = int(i/6)
-            columnarChunk = encryptKey[columnLoc*6:(columnLoc+1)*6]
-
-            #going to try to print out the '5'
-            if i-columnLoc < (columnLoc+1)*6:
-                columnLocation = i-leftMove+6
-                print("encryptKey: ", encryptKey[columnLocation])
-            else:
-                columnLocation = i-leftMove
-                print("encryptKey: ", encryptKey[columnLocation])
 
 
 
-            #so now we take the column location and use it to print out from the list, this will also need to wraparound
-            print(columnLocation)
+            columnLocation = findColumn(char)
+            print("Column Location: ", columnLocation)
+            print("upMove", upMove)
 
-            print(encryptKey[columnLocation])
-            rowLocation = columnLocation - (upMove*6)
-            print(encryptKey[rowLocation])
+            rowLocation = findRow(columnLocation)
+            print("rowLocation: ", encryptKey[rowLocation])
+            print(encryptKey[-12])
 
 
-            #if columnLocation - (upMove*6) < 0:
+            #at this point we still need to shift everything up
+            #and then shift everything to the left
+            #and then move the marker
+            #and also append everything to a new list, which will be our output
+            #we will use the decrypted string for this
+
+
+
+            decryptedString += encryptKey[rowLocation]
+            print("decryptedString: ", decryptedString)
+
+
+
+
+
 
 
 
