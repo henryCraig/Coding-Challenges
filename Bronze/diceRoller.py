bca46e4d108d1e7b0152ccf9142dd0f8afe31123
Henry Craig <h.craig52@yahoo.com>
2018-11-05 13:26:22 -0500

    Update diceRoller.py

--- Bronze/diceRoller.py
+++ Bronze/diceRoller.py
@@ -1,27 +1,21 @@
 #https://www.reddit.com/r/dailyprogrammer/comments/8s0cy1/20180618_challenge_364_easy_create_a_dice_roller/
 import random
 
-#TODO: add in multi arg functionality
-
 def diceRoller(diceString):
-    #print(2*int(diceString[0]))
     iterator = 0
     randList = []
-    #for i number of Arguments repeat this loop
-    for i in range(int(diceString[0])):
-        randomNum = random.randint(1,int(diceString[2:]))
-        print(randomNum)
+    dLoc = diceString.find("d")
+
+
+    for i in range(int(diceString[:dLoc])):
+        randomNum = random.randint(1,int(diceString[dLoc+1:]))
         randList.append(randomNum)
         iterator += randomNum
-        print(randList)
 
     #TODO: this is some kind of loop which will take the list and impart it onto this string
 
+    print(iterator, ":", str(randList))
     return iterator
-    print(randList)
-    #print(iterator, ": ", )
 
 
-
-
-diceRoller('4d8')
+diceRoller('10d18')
