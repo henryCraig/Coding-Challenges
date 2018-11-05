2ba36f4f4bb402aa80e08a6d9982fe30ffc64a08
Henry Craig <h.craig52@yahoo.com>
2018-11-05 13:27:03 -0500

    Update diceRoller.py

--- Bronze/diceRoller.py
+++ Bronze/diceRoller.py
@@ -12,9 +12,8 @@ def diceRoller(diceString):
         randList.append(randomNum)
         iterator += randomNum
 
-    #TODO: this is some kind of loop which will take the list and impart it onto this string
-
-    print(iterator, ":", str(randList))
+        
+    print(iterator, ":", randList)
     return iterator
 
 
